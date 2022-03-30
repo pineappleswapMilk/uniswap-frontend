ef9f16ea7a33a03ef5f1f8be2ccdba63d1c07722
PineappleSwap <pineappleswap@mail.com>
2022-03-31 05:15:20 +1030

    m

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -347,7 +347,7 @@ export default function Header() {
           <StyledNavLink id={`stake-nav-link`} to={'/vote'}>
             Vote
           </StyledNavLink>
-          <StyledExternalLink id={`stake-nav-link`} href={'https://uniswap.info'}>
+          <StyledExternalLink id={`stake-nav-link`} href={'https://PineappleSwap.io'}>
             Charts <span style={{ fontSize: '11px' }}>↗</span>
           </StyledExternalLink>
         </HeaderLinks>
